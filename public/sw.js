2a16252e68d4dc0f5621341da500a611857b48d7
LilianKalkman <liliankalkman@hotmail.com>
2018-04-24 10:36:10 +0200

    precaching static

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,13 @@
 
 self.addEventListener('install', function(event) {
   console.log('[Service Worker] Installing Service Worker ...', event);
+  event.waitUntil(
+    caches.open('static')
+    .then(function(cache){
+      console.log('[Service Worker] Precaching App Shell');
+      cache.add('/src/js/app.js')
+    })
+  )
 });
 
 self.addEventListener('activate', function(event) {
@@ -9,5 +16,14 @@ self.addEventListener('activate', function(event) {
 });
 
 self.addEventListener('fetch', function(event) {
-  event.respondWith(fetch(event.request));
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response){
+        if(response){
+          return response;
+        } else {
+          return fetch(event.request);
+        }
+      })
+  );
 });
