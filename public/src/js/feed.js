1b8b66d11dbc42f3a4fdb62b34250aa4050f0b2d
LilianKalkman <liliankalkman@hotmail.com>
2018-04-24 13:35:06 +0200

    cache addall static precach

--- public/src/js/feed.js
+++ public/src/js/feed.js
@@ -52,10 +52,12 @@ function createCard() {
   sharedMomentsArea.appendChild(cardWrapper);
 }
 
-fetch('https://httpbin.org/get')
-  .then(function(res) {
-    return res.json();
-  })
-  .then(function(data) {
-    createCard();
-  });
+// fetch('https://httpbin.org/get')
+//   .then(function(res) {
+//     return res.json();
+//   })
+//   .then(function(data) {
+//     createCard();
+//   });
+
+createCard();
