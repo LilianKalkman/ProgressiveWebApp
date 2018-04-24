8b879f730a2d38da99f38891775921bcf07cf2b5
LilianKalkman <liliankalkman@hotmail.com>
2018-04-24 09:53:57 +0200

    updated

--- public/sw.js
+++ public/sw.js
@@ -1,13 +1,13 @@
-self.addEventListener('install', function(event){
-  console.log('[service worker] installing ...', event);
+
+self.addEventListener('install', function(event) {
+  console.log('[Service Worker] Installing Service Worker ...', event);
 });
 
-self.addEventListener('activate', function(event){
-  console.log('[service worker] activating ...', event);
+self.addEventListener('activate', function(event) {
+  console.log('[Service Worker] Activating Service Worker ....', event);
   return self.clients.claim();
 });
 
-self.addEventListener('fetch', function(event){
-  console.log('[SW] fetching something ...', event);
+self.addEventListener('fetch', function(event) {
   event.respondWith(fetch(event.request));
 });
