44b9dcff95c28d8860d4e774955a7b3bed9da1a8
LilianKalkman <liliankalkman@hotmail.com>
2018-05-02 12:19:31 +0200

    dynamic caching from firebase and UI update dynamic

--- public/src/js/feed.js
+++ public/src/js/feed.js
@@ -42,23 +42,23 @@ function onSaveButtonClicked(event){
   }
 }
 
-function createCard() {
+function createCard(data) {
   var cardWrapper = document.createElement('div');
   cardWrapper.className = 'shared-moment-card mdl-card mdl-shadow--2dp';
   var cardTitle = document.createElement('div');
   cardTitle.className = 'mdl-card__title';
-  cardTitle.style.backgroundImage = 'url("/src/images/bali.JPG")';
+  cardTitle.style.backgroundImage = 'url(' + data.image + ')';
   cardTitle.style.backgroundSize = 'cover';
   cardTitle.style.height = '180px';
   cardWrapper.appendChild(cardTitle);
   var cardTitleTextElement = document.createElement('h2');
   cardTitleTextElement.className = 'mdl-card__title-text';
-  cardTitleTextElement.textContent = 'Bali Trip';
+  cardTitleTextElement.textContent = data.title;
   cardTitleTextElement.style.color = 'white';
   cardTitle.appendChild(cardTitleTextElement);
   var cardSupportingText = document.createElement('div');
   cardSupportingText.className = 'mdl-card__supporting-text';
-  cardSupportingText.textContent = 'In Bali';
+  cardSupportingText.textContent = data.location;
   cardSupportingText.style.textAlign = 'center';
   // var cardSaveButton = document.createElement('button');
   // cardSaveButton.textContent = 'Save';
@@ -69,13 +69,51 @@ function createCard() {
   sharedMomentsArea.appendChild(cardWrapper);
 }
 
+function clearCards() {
+  while(sharedMomentsArea.hasChildNodes()) {
+    sharedMomentsArea.removeChild(sharedMomentsArea.lastChild);
+  }
+}
 
-createCard();
+function updateUI(data){
+  clearCards();
+  for (var i = 0; i < data.length; i++){
+    createCard(data[i]);
+  }
+}
 
-// fetch('https://httpbin.org/get')
-//   .then(function(res) {
-//     return res.json();
-//   })
-//   .then(function(data) {
-//     createCard();
-//   });
+var url = 'https://l-ilstagram.firebaseio.com/posts.json';
+var networkDataReceived = false;
+
+fetch(url)
+  .then(function(res) {
+    return res.json();
+  })
+  .then(function(data) {
+    networkDataReceived = true;
+    console.log('From web', data);
+    var dataArray = [];
+    for (var key in data){
+      dataArray.push(data[key]);
+    };
+    updateUI(dataArray);
+  });
+
+if ('caches' in window) {
+  caches.match(url)
+    .then(function(response) {
+      if (response) {
+        return response.json();
+      }
+    })
+    .then(function(data) {
+      console.log('From cache', data);
+      if (!networkDataReceived) {
+        var dataArray = [];
+        for (var key in data){
+          dataArray.push(data[key]);
+        };
+        updateUI(dataArray);
+      }
+    });
+}
